6d0305f21d0ef42c19db192c41e049bd468deb23
Dayron Diaz <dayron.diaz@bbconsult.co.uk>
2023-02-26 15:28:49 -0500

    add dron model and dron status configuration

---
+++ DroneManager.Infrastructure/Configurations/DronModelConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using DroneManager.Core.Data.Configurations;
+using DroneManager.Domain.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DroneManager.Infrastructure.Configurations
+{
+    public class DronModelConfiguration : BaseEnumerationConfiguration<DronModel>
+    {
+        public override void Configure(EntityTypeBuilder<DronModel> builder)
+        {
+            base.Configure(builder);
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Name)
+                .HasMaxLength(32);
+        }
+    }
+}
---
+++ DroneManager.Infrastructure/Configurations/DronStatusConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using DroneManager.Core.Data.Configurations;
+using DroneManager.Domain.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DroneManager.Infrastructure.Configurations
+{
+    public class DronStatusConfiguration : BaseEnumerationConfiguration<DronStatus>
+    {
+        public override void Configure(EntityTypeBuilder<DronStatus> builder)
+        {
+            base.Configure(builder);
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Name)
+                .HasMaxLength(32);
+        }
+    }
+}
