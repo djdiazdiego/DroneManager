5083bab5525ab8e4a1b422e785922ebd0fd5ddf3
Dayron Diaz <dayron.diaz@bbconsult.co.uk>
2023-02-26 15:22:28 -0500

    add dron configuration

--- DroneManager.Domain/Models/Dron.cs
+++ DroneManager.Domain/Models/Dron.cs
@@ -6,10 +6,13 @@ namespace DroneManager.Domain.Models
     public class Dron : Entity<int>
     {
         public string SerialNumber { get; set; }
-        public DronModelValues Model { get; set; }
+        public DronModelValues ModelId { get; set; }
         public double Weight { get; set; }
         public decimal BatteryCapacity { get; set; }
-        public DronStatusValues Status { get; set; }
+        public DronStatusValues StatusId { get; set; }
+
+        public DronModel Model { get; set; }
+        public DronStatus Status { get; set; }
 
     }
 }
---
+++ DroneManager.Infrastructure/Configurations/DronConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using DroneManager.Core.Data.Configurations;
+using DroneManager.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DroneManager.Infrastructure.Configurations
+{
+    public class DronConfiguration : BaseEntityConfiguration<Dron>
+    {
+        public override void Configure(EntityTypeBuilder<Dron> builder)
+        {
+            base.Configure(builder);
+
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Id).ValueGeneratedOnAdd();
+
+            builder.Property(p => p.SerialNumber).HasMaxLength(100);
+
+            builder.Property(p => p.BatteryCapacity).HasPrecision(5, 2);
+
+            builder.HasOne(p => p.Model)
+                .WithMany()
+                .HasForeignKey(p => p.ModelId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(p => p.Status)
+                .WithMany()
+                .HasForeignKey(p => p.StatusId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
